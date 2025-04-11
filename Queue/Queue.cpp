a491b1fce608a871e4c5ff249c46d4bcc1aa00eb
CzikaS <czikastefania@yahoo.com>
2025-04-11 13:57:12 +0300

    constructor update +  allocate + free + resize

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,6 +1,7 @@
 #include "Queue.h"
 #include <exception>
 #include <iostream>
+#include <random>
 
 using namespace std;
 
@@ -10,12 +11,14 @@ Queue::Queue() {
 	capacity = 10;
 	size = 0;
 	head = tail = -1;
-	firstEmpty = -1;
+	firstEmpty = 0;
 	nodes = new DLLANode[capacity]();
-	int index = 1;
-	for (auto node : nodes) {
-		node.next = index++;
+	for (int i = 0; i < capacity - 1; i++) {
+		nodes[i].next = i + 1;
+		nodes[i].prev = -1;
 	}
+	nodes[capacity - 1].next = -1;
+	nodes[capacity - 1].prev = -1;
 }
 
 
@@ -31,7 +34,6 @@ TElem Queue::top() const {
 
 TElem Queue::pop() {
 	//TODO - Implementation
-
 	return NULL_TELEM;
 }
 
@@ -46,12 +48,38 @@ Queue::~Queue() {
 	//TODO - Implementation
 }
 
-int Queue::allocate() {
+int Queue::allocate() { // returneaza indicele la care trebuie alocata noua valoare
+	if (firstEmpty == -1)
+		return -1;
+	int newElem = firstEmpty;
+	firstEmpty = nodes[firstEmpty].next;
+	if (firstEmpty != -1)
+		nodes[firstEmpty].prev = -1;
+	return newElem;
 }
 
-void Queue::free(int position) {
+void Queue::free(int position) { // adauga un nod in "lista de noduri libere"
+	nodes[position].next = firstEmpty;
+	nodes[position].prev = -1;
+	if (firstEmpty != -1)
+		nodes[firstEmpty].prev = position;
+	firstEmpty = position;
 }
 
-void Queue::resize() {
-}
+void Queue::resize() { // dubleaza capacitatea si reface lista de noduri libere
+	int newCapacity = capacity*2;
+	DLLANode* newNodes = new DLLANode[newCapacity];
+	for (int i = 0; i < newCapacity; i++)
+		newNodes[i] = nodes[i];
+	for (int i = capacity; i < newCapacity-1; i++) {
+		newNodes[i].next = i + 1;
+		newNodes[i].prev = -1;
+	}
+	newNodes[newCapacity-1].next = -1;
+	newNodes[newCapacity-1].prev = -1;
+	firstEmpty = capacity;
 
+	delete[] nodes;
+	nodes = newNodes;
+	capacity = newCapacity;
+}
