d727613aee842385ab5cebf2c84272a9b7f2c30f
CzikaS <czikastefania@yahoo.com>
2025-04-11 15:20:47 +0300

    labyrinth constructor + added labyrinth.txt

--- Labyrinth/Labyrinth.cpp
+++ Labyrinth/Labyrinth.cpp
@@ -3,3 +3,58 @@
 //
 
 #include "Labyrinth.h"
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <fstream>
+
+using namespace std;
+
+Labyrinth::Labyrinth(const string& filename) {
+    ifstream file;
+    file.open(filename);
+    if (!file) {
+        throw std::runtime_error("File not found: " + filename);
+    }
+
+    string line;
+    vector<vector<char>> tempGrid;
+    bool startFound = false;
+    int rowIdx = 0;
+
+    while (getline(file,line)) {
+        vector<char> row;
+        stringstream ss(line);
+        string cell;
+        int colIdx = 0;
+
+        while (getline(ss, cell, '\t')) {
+            if (cell.empty()) continue;
+            char symbol = cell[0];
+            row.push_back(symbol);
+
+            if (symbol == 'R') {
+                start = {rowIdx, colIdx};
+                startFound = true;
+            }
+            colIdx++;
+        }
+
+        if (!row.empty()) {
+            tempGrid.push_back(row);
+            rowIdx++;
+        }
+    }
+    file.close();
+
+    if (tempGrid.empty()) {
+        throw std::runtime_error("Labyrinth is empty");
+    }
+    if (!startFound) {
+        throw std::runtime_error("Start position R not found");
+    }
+
+    grid = tempGrid;
+    rows = grid.size();
+    cols = grid[0].size();
+}
